44951025be47fb1027ae27fbfa925467bf26e3e3
Michael Fleming <flemingatx@gmail.com>
2016-06-30 12:11:15 -0500

    thing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/Public'));
+//app.use(express.static(__dirname + '/Public'));
 
 app.get('/', function(request, response) {
   response.sendFile('click-party.html');
@@ -13,5 +13,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
