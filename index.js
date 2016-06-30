a488df57655ad07c49e3dbe2c1dc8e9d07e3b0d4
Michael Fleming <flemingatx@gmail.com>
2016-06-30 12:08:43 -0500

    added server, gitignore, etc

---
+++ index.js
@@ -0,0 +1,17 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', function(request, response) {
+  response.sendFile('click-party.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
