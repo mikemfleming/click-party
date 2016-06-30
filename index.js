bfbe91d8afa931161bfc31e61078f1a442dd4564
Michael Fleming <flemingatx@gmail.com>
2016-06-30 12:31:18 -0500

    modified path in index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var path = require('path');
+
+var main = path.join(__dirname, '/click-party.html')
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/Public'));
 
 app.get('/', function(request, response) {
-  response.sendFile('/Users/michaelfleming/Documents/Projects/click-party/click-party.html');
+	console.log('all up in your get ~~~~~~~~~')
+  response.sendFile(main);
 });
 
 app.listen(app.get('port'), function() {
