0580354671a13db389c80651e731606398ebe9ce
Michael Fleming <flemingatx@gmail.com>
2016-12-12 16:20:11 -0600

    adds grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,60 @@
 module.exports = function(grunt) {
+  // 1. All configuration goes here 
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+      concat: {   
 
-        concat: {   
-            dist: {
-                src: [
-                    './Public/click-party.js'
-                ],
-                dest: './Public/production.js',
-            }
+        dist: {
+          src: [
+              './Public/click-party.js'
+          ],
+          dest: './Public/production.js',
+        }
+
+      },
+
+      uglify: {
+
+        build: {
+          src: './Public/production.js',
+          dest: './Public/production.min.js'
+        }
+
+      },
+
+      cssmin: {
+
+        options: {
+          keepSpecialComments: 0
         },
 
-        uglify: {
-            build: {
-                src: './Public/production.js',
-                dest: './Public/production.min.js'
-            }
+        dist: {
+          files: {
+            './Public/click-party-stylesheet.min.css': './Public/click-party-stylesheet.css'
+          }
+        }
+        
+      },
+
+      watch: {
+        css: {
+          files: ['./Public/*.css'],
+          tasks: ['cssmin']
         },
+        js: {
+          files: ['./Public/*.js'],
+          tasks: ['concat', 'uglify']
+        }
+      },
+  });
 
-        cssmin: {
-
-              options: {
-                keepSpecialComments: 0
-              },
-              dist: {
-                files: {
-                  './Public/click-party-stylesheet.min.css': './Public/click-party-stylesheet.css'
-                }
-              }
-                  },
-
-        watch: {
-            scripts: {
-                files: ['js/*.js'],
-                tasks: ['concat', 'uglify'],
-                options: {
-                    spawn: false,
-                },
-            } 
-        },
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+  // 3. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'watch']);
 
 };
