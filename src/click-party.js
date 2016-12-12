613b60b3b99088be1113129c856124618bf11968
Michael Fleming <flemingatx@gmail.com>
2016-12-12 17:19:47 -0600

    adds color history

--- src/click-party.js
+++ src/click-party.js
@@ -1,17 +1,22 @@
 function runGame(){
 
+  var oldRand = window.getComputedStyle(document.body).getPropertyValue('--randomColor');
+  var oldComp = window.getComputedStyle(document.body).getPropertyValue('--compColor');
+
+  document.querySelector('#history').innerHTML += 
+    '<span style="color:' + oldRand + '">' + oldRand + '</span>' + ' '
+    + '<span style="color:' + oldComp + '">' + oldComp+ '</span> ';
+  // uglify doesn't like template strings :(
+
   var randHex = "000000".replace(/0/g,function(){
     return (~~(Math.random()*16)).toString(16);
-  });
-  var compHex = decToHex(0xFFFFFF - ("0x" + randHex))
+  }).toUpperCase();
+  var compHex = decToHex(0xFFFFFF - ("0x" + randHex));
 
-  document.body.style.setProperty('--randomColor', '#' + randHex); // uglify doesn't like template strings :(
+  document.body.style.setProperty('--randomColor', '#' + randHex);
   document.body.style.setProperty('--compColor', compHex);
   
-  document.querySelector('.hexText').innerHTML = '#' + randHex.toUpperCase() + ' : ' + compHex.toUpperCase()
-
-  console.log("This is the background hex color: " + randHex);
-  console.log("This is the text color: " + compHex);
+  document.querySelector('.hexText').innerHTML = '#' + randHex.toUpperCase() + ' : ' + compHex.toUpperCase();
   
   function decToHex ( d ) {
     var c = Number(d).toString(16);
@@ -19,6 +24,6 @@ function runGame(){
   }
 }
 
-document.addEventListener('click', runGame)
-document.addEventListener('keypress', runGame)
-document.querySelector('.container').addEventListener('click', runGame) // helps with mobile
+document.addEventListener('click', runGame);
+document.addEventListener('keypress', runGame);
+document.querySelector('#container').addEventListener('click', runGame); // helps with mobile
